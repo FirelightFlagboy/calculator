30081f5fda3d6ba13cb6f3a077cf485e4d518715
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-02-22 22:16:06 +0100

    work on parser

--- srcs/parser/ft_parse_tree.c
+++ srcs/parser/ft_parse_tree.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <f.benneto@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 21:36:25 by fbenneto          #+#    #+#             */
-/*   Updated: 2018/02/22 22:15:33 by fbenneto         ###   ########.fr       */
+/*   Updated: 2018/02/22 22:16:03 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,25 @@
 **  /\  /\
 **  2 3 4 5
 */
-void		parse_factor_tree(t_token **head, t_tree **tree)
-{
-	parse_factor_tree(head, tree);
-}
+// void		parse_factor_tree(t_token **head, t_tree **tree)
+// {
+// 	parse_factor_tree(head, tree);
+// }
 
-void		parse_product_tree(t_token **head, t_tree **tree)
-{
-	parse_product_tree(head, tree);
-}
+// void		parse_product_tree(t_token **head, t_tree **tree)
+// {
+// 	parse_product_tree(head, tree);
+// }
 
-void		parse_sum_tree(t_token **head, t_tree **tree)
-{
-	parse_product_tree(head, tree);
-}
+// void		parse_sum_tree(t_token **head, t_tree **tree)
+// {
+// 	parse_product_tree(head, tree);
+// }
 
-t_tree		*parser_tree(t_token **head)
-{
-	t_tree *tree;
+// t_tree		*parser_tree(t_token **head)
+// {
+// 	t_tree *tree;
 
-	parse_sum_tree(head, &tree);
-	return (tree);
-}
+// 	parse_sum_tree(head, &tree);
+// 	return (tree);
+// }

80e745670a146f5d5758245b52357d2b41005de1
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-02-24 11:21:14 +0100

    wrok on parser tree

--- srcs/parser/print_tree.c
+++ srcs/parser/print_tree.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <f.benneto@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 21:51:33 by fbenneto          #+#    #+#             */
-/*   Updated: 2018/02/24 11:19:16 by fbenneto         ###   ########.fr       */
+/*   Updated: 2018/02/24 11:21:11 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,22 @@ static void	ft_printf_ast_util(t_tree *head, int space, int count)
 	space += 5;
 	ft_printf_ast_util(head->high, space, count + 1);
 	if (head->high)
+	{
+		for (i = 5; i < space; i++)
+			printf(" ");
 		printf("/");
+	}
 	printf("\n");
 	for (i = 5; i < space; i++)
 		printf(" ");
 
 	printf("%d:%s\n", count, head->val);
 	if (head->low)
+	{
+		for (i = 5; i < space; i++)
+			printf(" ");
 		printf("\\");
+	}
 	ft_printf_ast_util(head->low, space, count + 2);
 }
 
